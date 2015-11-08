55e4e13c1e027931514d79d86cfb5986b5afe5b9
kevinjcliao <me@liaokev.in>
2015-11-07 20:06:02 -0500

    Another attempt to get the bar working.

--- js/site.js
+++ js/site.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
   function onScroll() {
     if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
         $body.addClass('has-docked-nav');
-        $body.animate({backgroundColor: "#F06F6F"});
+        $('.navbar').animate({backgroundColor: "#F06F6F"});
     }
     if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
         $body.removeClass('has-docked-nav');
