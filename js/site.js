37f1fc8f245b4b67910b5364c60335ab7ce4a113
kevinjcliao <me@liaokev.in>
2015-11-07 20:21:19 -0500

    Make loading look a little less terrible.

--- js/site.js
+++ js/site.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
         $('.navbar').animate({backgroundColor: "#F06F6F"});
     }
     if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-        $('.navbar').animate({backgroundColor: "#FFFFFF"}, 2000,
+        $('.navbar').animate({backgroundColor: "#FFFFFF"}, 
                              function() {$body.removeClass('has-docked-nav');
                                          console.log("removing class.");
                                         });
